48b10115c2abdbce7c547a5d0b1be4a89ad52df7
POOM <nitipoom_t@cmu.ac.th>
2022-07-31 16:22:47 +0700

    2.9

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -6,6 +6,32 @@
       :key="event.id"
       :event="event"
     ></EventCard>
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{
+          name: 'EventList',
+          query: { page: page - 1 }
+        }"
+        rel="prev"
+        v-if="page != 1"
+      >
+        Prev Page</router-link
+      >
+    </div>
+    <div class="pagination">
+      <router-link
+        id="page-next"
+        :to="{
+          name: 'EventList',
+          query: { page: page + 1 }
+        }"
+        rel="next"
+        v-if="hasNextPage"
+      >
+        Next Page</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -13,24 +39,48 @@
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import { watchEffect } from '@vue/runtime-core'
 export default {
   name: 'EventListView',
+  props: {
+    page: {
+      type: Number,
+      required: true
+    },
+    perPage: {
+      type: Number,
+      required: true
+    }
+  },
   components: {
     EventCard
   },
   data() {
     return {
-      events: null
+      events: null,
+      totalEvents: 0 //<--- Added this to store totalEvents
     }
   },
   created() {
-    EventService.getEvents()
-      .then((response) => {
-        this.events = response.data
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    watchEffect(() => {
+      EventService.getEvents(this.perPage, this.page)
+        .then((response) => {
+          this.events = response.data
+          this.totalEvents = response.headers['x-total-count'] //<--- Store it
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    })
+  },
+  computed: {
+    hasNextPage() {
+      //First, calculate total pages
+      let totalPages = Math.ceil(this.totalEvents / this.perPage) // 2 is events per page
+
+      //Then check to see if the current page is less than the total pages
+      return this.page < totalPages
+    }
   }
 }
 </script>
@@ -39,5 +89,24 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
